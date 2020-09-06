936bef17961f08b1c577cfbc08dd412564a9de8f
Jaden <theamericanyank@gmail.com>
2020-09-05 20:43:54 -0400

    Working in conversions
    check sanity still not working due to bug with .slice
    but everything converts correctly

--- romanconvertor.rb
+++ romanconvertor.rb
@@ -1,281 +1,268 @@
 
 
 def fromRoman(roman_number)
-  # Replace the following line with the actual code!
-  allowed_romans = Array.[]('I', 'V', 'X', 'L', 'C', 'D', 'M')
-  subtractions = Array.[]('IV', 'IX', 'XL', 'CD', 'XC', 'CM')
-  special_case = false
-  puts 'fromRoman has input of ' + roman_number
-  character_array = roman_number.chars
+  original = roman_number
+  roman_dictionary = { 'I' => 1, 'V' => 5, 'X' => 10, 'L' => 50, 'C' => 100, 'D' => 500, 'M' => 1000 }
 
-  puts 'character array is ' + character_array.inspect
-  subtractions.each do |x|
-    if roman_number.include? x
-      special_case = true
-      puts 'special case is ' + special_case.to_s
-      puts 'this is a special case with ' + x
-      subtractive_notation_case(character_array, roman_number, subtractions, allowed_romans)
-      break
-    end
-  end
+  subtractions_dictionary = { 'IV' => 4, 'IX' => 9, 'XL' => 40, 'XC' => 90, 'CD' => 400, 'CM' => 900 }
 
-  if special_case == false
-    non_subtractive_case(character_array, allowed_romans, 0)
-  end
+  # puts 'input is ' + roman_number
+  check_for_allowed(roman_number, roman_dictionary)
+  compute_values(roman_number, subtractions_dictionary, roman_dictionary)
+
+
+
 end
 
-def non_subtractive_case(character_array, allowed_romans, additional)
-  total = 0
+def check_for_allowed(input, dictonary)
+  character_array = input.chars
   character_array.each do |char|
-    if allowed_romans.include? char
-      puts char + ' is in array'
-      total += convert_roman_to_number(char)
-    else
-      puts char + ' is not in the array'
+    unless dictonary.include? char
       raise TypeError
     end
   end
-  total += additional
-  puts total
-  puts 'The Class of the return value is '
-  puts total.class
+end
+
+def has_subtractions(roman_number, subtractions_dictionary)
+  duplicate = roman_number
+  x = 0
+  y = 1
+  has_subtractions = %w[]
+  until y == roman_number.length
+    if subtractions_dictionary.include? roman_number[x] + roman_number[y]
+      has_subtractions.push(roman_number[x]+roman_number[y])
+      x += 1
+      y += 1
+    else
+      x += 1
+      y += 1
+    end
+  end
+  #the problem is because this .slice code is screwing up the orignial somehow
+  has_subtractions.each do |subtraction|
+    puts has_subtractions.inspect
+    if roman_number.include? subtraction
+      roman_number.slice!(subtraction)
+      puts 'roman number after slice ' + roman_number
+    end
+  end
+  # puts 'string is now ' + roman_number
+  puts 'the subtractions are ' + has_subtractions.inspect
+  return compute_subtractions(has_subtractions, subtractions_dictionary)
+end
+
+def compute_subtractions(subtractions, dictionary)
+  # puts 'computing subtractions'
+  # puts subtractions.inspect
+  total = 0
+  subtractions.each do |subtraction|
+    total += dictionary.fetch(subtraction)
+  end
+  # puts 'subtractions total is ' + total.to_s
   total
 end
 
-def subtractive_notation_case(character_array, roman_number, subtractions, allowed_romans)
-  puts 'running subtractive case'
-  puts character_array.inspect
-  subtraction_cases = Array.[]
-
-  subtractions.each do |x|
-    if roman_number.include? x
-      puts 'this is a special case with ' + x
-      subtraction_cases.push(x)
-      roman_number.slice! x
-    end
+def compute_values(roman_number, subtractions_dictionary, roman_dictionary)
+  total = 0
+  # puts 'right before adding subtractions the array is ' + roman_number.to_s
+  total += has_subtractions(roman_number, subtractions_dictionary)
+  puts 'roman number during final compute is ' + roman_number
+  roman_number.chars.each do |roman|
+    total += roman_dictionary.fetch(roman)
   end
-  character_array = roman_number.chars
-  puts 'our cases are now ' + subtraction_cases.inspect
-  puts 'our string is now ' + roman_number
-  subtractive_total = 0
-
-  #'IV', 'IX', 'XL', 'CD', 'XC', 'CM'
-  subtraction_cases.each do |y|
-    if y == 'IV'
-      subtractive_total += 4
-    elsif y == 'IX'
-      subtractive_total += 9
-    elsif y == 'XL'
-      subtractive_total += 40
-    elsif y == 'CD'
-      subtractive_total += 400
-    elsif y == 'XC'
-      subtractive_total += 90
-    elsif y == 'CM'
-      subtractive_total += 900
-    end
-  end
-
-  non_subtractive_case(character_array, allowed_romans, subtractive_total)
-
-end
-
-def convert_roman_to_number(roman_character)
-  if roman_character == 'I'
-    puts 'the character is I'
-    value = 1
-    return value
-  elsif roman_character == 'V'
-    puts 'the character is V'
-    value = 5
-    return value
-  elsif roman_character == 'X'
-    puts 'the character is X'
-    value = 10
-    return value
-  elsif roman_character == 'L'
-    puts 'the character is L'
-    value = 50
-    return value
-  elsif roman_character == 'C'
-    puts 'the character is C'
-    value = 100
-    return value
-  elsif roman_character == 'D'
-    puts 'the character is D'
-    value = 500
-    return value
-  elsif roman_character == 'M'
-    puts 'the character is M'
-    value = 1000
-    return value
-  else
-    abort("guess I'll just die")
-  end
+  # puts 'total is ' + total.to_s
+  return total
 end
 
 def toRoman(arabic_number)
-  # Replace the following line with the actual code!
-  puts 'toRoman has input of ' + arabic_number.to_s
+  roman_dictionary = { 1 => 'I', 5 => 'V', 10 => 'X', 50 => 'L', 100 => 'C', 500 => 'D', 1000 => 'M' }
 
-  if arabic_number.negative? || arabic_number > 3999 || arabic_number.zero?
-    puts 'Number is outside of range'
-    raise RangeError
-  else
-    puts 'number is in range'
-    convert_number_to_roman(arabic_number)
-  end
-
+  subtractions_dictionary = { 4 => 'IV', 9 => 'IX', 40 => 'XL', 90 => 'XC', 400 => 'CD', 900 => 'CM' }
+  to_roman_check_range(arabic_number)
+  compute_to_roman(arabic_number, roman_dictionary, subtractions_dictionary)
 end
 
-def convert_number_to_roman(number)
-  puts 'new function with input of ' + number.to_s
-  if number < 5
-    roman_string = ''
-    number.times do
-      roman_string += 'I'
-    end
-
-  elsif number == 5
-    roman_string = 'V'
-
-  elsif number == 10
-    roman_string = 'X'
-
-  elsif number == 50
-    roman_string = 'L'
-
-  elsif number == 100
-    roman_string = 'C'
-
-  elsif number == 500
-    roman_string = 'D'
-
-  elsif number == 1000
-    roman_string = 'M'
-
-  else
-    roman_string = ''
-    if (number - 1000).positive? || (number - 1000).zero?
-      number = number - 1000
-      next_roman = 'M'
-      roman_string += next_roman
-      puts 'adding M string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    until (number - 900).negative? || (number - 900).zero?
-      number = number - 900
-      next_roman = 'CM'
-      roman_string += next_roman
-      puts 'adding CM string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    until (number - 500).negative? || (number - 500).zero?
-        number = number - 500
-        next_roman = 'D'
-        roman_string += next_roman
-        puts 'adding D string is now ' + roman_string
-        puts 'number is now ' + number.to_s
-    end
-
-    until (number - 400).negative? || (number - 400).zero?
-      number = number - 400
-      next_roman = 'CD'
-      roman_string += next_roman
-      puts 'adding CD string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    until (number - 100).negative? || (number - 100).zero?
-        number = number - 100
-        next_roman = 'C'
-        roman_string += next_roman
-        puts 'adding C string is now ' + roman_string
-        puts 'number is now ' + number.to_s
-    end
-
-    until (number - 90).negative? || (number - 90).zero?
-      number = number - 90
-      next_roman = 'XC'
-      roman_string += next_roman
-      puts 'adding XC string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    until (number - 50).negative? || (number - 50).zero?
-        number = number - 50
-        next_roman = 'L'
-        roman_string += next_roman
-        puts 'adding L string is now ' + roman_string
-        puts 'number is now ' + number.to_s
-    end
-
-    until (number - 40).negative? || (number - 40).zero?
-      number = number - 40
-      next_roman = 'XL'
-      roman_string += next_roman
-      puts 'adding XL string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    until (number - 10).negative? || (number - 10).zero?
-        number = number - 10
-        next_roman = 'X'
-        roman_string += next_roman
-        puts 'adding X string is now ' + roman_string
-        puts 'number is now ' + number.to_s
-    end
-
-    until (number - 9).negative? || (number - 9).zero?
-      number = number - 9
-      next_roman = 'IX'
-      roman_string += next_roman
-      puts 'adding IX string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    until (number - 5).negative?
-        number = number - 5
-        next_roman = 'V'
-        roman_string += next_roman
-        puts 'adding V string is now ' + roman_string
-        puts 'number is now ' + number.to_s
-    end
-
-    until (number - 4).negative? || (number - 4).zero?
-      number = number - 4
-      next_roman = 'IV'
-      roman_string += next_roman
-      puts 'adding IV string is now ' + roman_string
-      puts 'number is now ' + number.to_s
-    end
-
-    puts number
-    number.times do
-      roman_string += 'I'
-    end
-    puts 'adding I string is now ' + roman_string
-
-
-
+def to_roman_check_range(input)
+  if input.negative? || input > 3999 || input.equal?(0)
+    raise RangeError
   end
-  puts roman_string
+end
+
+def compute_to_roman(input, roman_dictionary, subtractions_dictionary)
+  if roman_dictionary.include? input
+    puts 'already in dictionary '
+    roman_dictionary.fetch(input)
+  elsif subtractions_dictionary.include? input
+    subtractions_dictionary.fetch(input)
+  else
+    # puts 'running the else case because it is a complex roman'
+    to_roman_subtractions_check(input, roman_dictionary, subtractions_dictionary)
+  end
+end
+
+def to_roman_subtractions_check(input, roman_dictionary, subtractions_dictionary)
+  # puts 'Checking for subtraction cases'
+  split_number_array = input.to_s.chars
+  subtractions_case = false
+  index = 0
+  split_number_array.each do
+    if split_number_array[index].to_i.equal?(4) || split_number_array[index].to_i.equal?(9)
+      subtractions_case = true
+    else
+      index += 1
+    end
+  end
+
+  if subtractions_case
+    solve_subtractions_case(input, subtractions_dictionary, roman_dictionary)
+  else
+    solve_simple_roman(input, roman_dictionary)
+  end
+end
+
+def solve_subtractions_case(input, subtractions_dictionary, roman_dictionary)
+  # puts 'solving for subtractions case'
+  final_roman = ''
+  subtraction_place_check = input.to_s.chars
+  counter = 0
+  subtraction_place_check.each do
+    subtraction_place_check[counter] = subtraction_place_check[counter].to_i
+    counter += 1
+  end
+  # puts subtraction_place_check.inspect
+
+  if subtraction_place_check.length.equal?(2)
+    puts 'length is 2'
+    final_roman = solve_subtractions_in_10(subtraction_place_check, roman_dictionary)
+
+  elsif subtraction_place_check.length.equal?(3)
+    puts 'length is 3'
+    final_roman = solve_subtractions_in_100(subtraction_place_check, roman_dictionary)
+
+  elsif subtraction_place_check.length.equal?(4)
+    # puts 'length is 4'
+    subtraction_place_check[0] = subtraction_place_check[0].to_s << '000'
+    subtraction_place_check[0] = subtraction_place_check[0].to_i
+    final_roman << solve_simple_roman(subtraction_place_check[0], roman_dictionary)
+    subtraction_place_check.shift
+    # puts subtraction_place_check.inspect
+    final_roman << solve_subtractions_in_100(subtraction_place_check, roman_dictionary)
+  end
+
+  final_roman #return final_roman
+end
+
+def solve_subtractions_in_100(subtraction_place_check, roman_dictionary)
+  # puts 'solving in 100 position'
+  final_roman = ''
+  if subtraction_place_check[0].equal?(4) || subtraction_place_check[0].equal?(9)
+    if subtraction_place_check[0].equal?(4)
+      final_roman << 'CD'
+    else
+      final_roman << 'CM'
+    end
+  else
+    subtraction_place_check[0] = subtraction_place_check[0].to_s << '00'
+    subtraction_place_check[0] = subtraction_place_check[0].to_i
+    final_roman << solve_simple_roman(subtraction_place_check[0], roman_dictionary)
+  end
+  if subtraction_place_check[1].equal?(4) || subtraction_place_check[1].equal?(9)
+    if subtraction_place_check[1].equal?(4)
+      final_roman << 'XL'
+    else
+      final_roman << 'XC'
+    end
+  else
+    subtraction_place_check[1] = subtraction_place_check[1].to_s << '0'
+    subtraction_place_check[1] = subtraction_place_check[1].to_i
+    final_roman << solve_simple_roman(subtraction_place_check[1], roman_dictionary)
+  end
+  if subtraction_place_check[2].equal?(4) || subtraction_place_check[2].equal?(9)
+    if subtraction_place_check[2].equal?(4)
+      final_roman << 'IV'
+    else
+      final_roman << 'IX'
+    end
+  else
+    final_roman << solve_simple_roman(subtraction_place_check[2], roman_dictionary)
+  end
+  final_roman
+end
+
+def solve_subtractions_in_10(subtraction_place_check, roman_dictionary)
+  final_roman = ''
+  # puts 'solving in 10 position'
+  if subtraction_place_check[0].equal?(4) || subtraction_place_check[0].equal?(9)
+    if subtraction_place_check[0].equal?(4)
+      final_roman << 'XL'
+    else
+      final_roman << 'XC'
+    end
+  else
+    subtraction_place_check[0] = subtraction_place_check[0].to_s << '0'
+    subtraction_place_check[0] = subtraction_place_check[0].to_i
+    final_roman << solve_simple_roman(subtraction_place_check[0], roman_dictionary)
+  end
+  if subtraction_place_check[1].equal?(4) || subtraction_place_check[1].equal?(9)
+    if subtraction_place_check[1].equal?(4)
+      final_roman << 'IV'
+    else
+      final_roman << 'IX'
+    end
+  end
+  final_roman
+end
+
+def solve_simple_roman(input, roman_dictionary)
+  roman_string = ''
+  solve_simple_maths(input, roman_dictionary, roman_string)
+end
+
+def solve_simple_maths(input, roman_dictionary, roman_string)
+  if input >= 1000
+    roman_string << roman_dictionary.fetch(1000)
+    input -= 1000
+  elsif input >= 500
+    roman_string << roman_dictionary.fetch(500)
+    input -= 500
+  elsif input >= 100
+    roman_string << roman_dictionary.fetch(100)
+    input -= 100
+  elsif input >= 50
+    roman_string << roman_dictionary.fetch(50)
+    input -= 50
+  elsif input >= 10
+    roman_string << roman_dictionary.fetch(10)
+    input -= 10
+  elsif input >= 5
+    roman_string << roman_dictionary.fetch(5)
+    input -= 5
+  elsif input >= 1
+    roman_string << roman_dictionary.fetch(1)
+    input -= 1
+  else
+    puts 'something has gone horribly wrong'
+  end
+  unless input.equal?(0)
+    solve_simple_maths(input, roman_dictionary, roman_string)
+  end
   roman_string
 end
 
 
 
 
+# fromRoman('MCDXCVIII')
+# fromRoman('III')
 
-# fromRoman('XV')
-# fromRoman('VI')
-# fromRoman('LXXVIII')
-# fromRoman('CIII')
-#         15 => "XV",
-#         6 => "VI",
-#         78 => "LXXVIII",
-#         103 => "CIII"
-# toRoman(1498)
-fromRoman('MCDXCVIII')
+# x = toRoman(1999)
+# puts x
+# y = fromRoman(x)
+# puts y
+#
+original = 'MCDXCVIII'
+# original = 'IV'
+puts 'original ' + original
+x = fromRoman(original)
+puts 'original ' + original
+puts x
